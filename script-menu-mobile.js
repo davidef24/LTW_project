d7fbbee06bbc1b2fac0623e1c6297136061067ef
Emilio <emilioleo77@gmail.com>
2023-04-07 00:19:35 +0200

    Finita NavBar

---
+++ script-menu-mobile.js
@@ -0,0 +1,8 @@
+<script>
+    const menu_mobile=document.querySelector(".menu-mobile");
+    const nav_links=document.querySelector(".nav-links");
+    menu_mobile.addEventListener('click',()=>{nav_links.classList.toggle('mobile-menu')});   
+ </script>
+
+
+
