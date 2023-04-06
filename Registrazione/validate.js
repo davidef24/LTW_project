ca360af442ba9ba58c63b750ef00ec750d724f71
davide.fortunato <fortunato.1936575@studenti.uniroma1.it>
2023-04-06 20:04:52 +0200

    progressi 6 aprile

---
+++ Registrazione/validate.js
@@ -0,0 +1,37 @@
+function emailIsEmpty(){
+    if (document.login.email.value==""){
+        alert("Inserire email");
+        return true;
+    }
+    return false;
+}
+function emailToRegister(){
+    if (document.registrazione.email.value==""){
+        alert("Inserire email");
+        return false;
+    }
+    else{
+        if (document.registrazione.email.value.match(/@/)) return true;
+        else{
+            alert("Email invalida");
+            return false;
+        }
+    }
+}
+function comparePws(){
+    var p1=document.registrazione.password1.value;
+    var p2=document.registrazione.password2.value;
+    if (p1=="" || p2==""){
+        alert("Inserire password");
+        return false;
+    }
+    if (p1!=p2){
+        alert("Le password inserite sono diverse");
+        return false;
+    }
+    return true;
+}
+function check_lr(){
+    if (!emailToRegister() || !comparePws() || emailIsEmpty()) return false;
+    return true;
+}
