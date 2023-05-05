f68c64309bc9885d0656cc18accf9cd53a275b8c
iorenzolacopino <lorenzo.iacopino2002@gmail.com>
2023-05-05 18:00:40 +0200

    validate.js

--- Registrazione/validate.js
+++ Registrazione/validate.js
@@ -1,68 +1,95 @@
-function validNameSurname(){
-    if (document.getElementById("nome").value=="" || document.getElementById("cognome").value==""){
-        alert("Inserire nome e cognome");
+function nameIsValid(){
+    if (document.getElementById("nome").value==""){
+        invalidName();
         return false;
     }
-    if (document.getElementById("nome").value.match(/^[a-zA-Z]+$/) && document.getElementById("cognome").value.match(/^[a-zA-Z]+$/)) return true;
-    else{
-        alert("Nome e congome non validi");
+    else validName();
+    if (!document.getElementById("nome").value.match(/^[a-zA-Z]+$/)){
+        invalidName();
         return false;
     }
+    validName();
+    return true;
+}
+function surnameIsValid(){
+    if (document.getElementById("cognome").value==""){
+        invalidSurname();
+        return false;
+    }
+    else validSurname();
+    if (!document.getElementById("cognome").value.match(/^[a-zA-Z]+$/)){
+        invalidSurname();
+        return false;
+    }
+    validSurname();
+    return true;
 }
 function validBirthDate(){
-    var today=new Date();
     var date=document.getElementById("nascita").value;
     if (date==""){
-        alert("Inserire data di nascita");
+        invalidDate();
         return false;
     }
+    validDate();
     var spl=date.split('-');
     var day=parseInt(spl[2]), month=parseInt(spl[1]), year=parseInt(spl[0]);
     if (year>2005 || (year==2005 && month >= 6)){
         alert("Devi essere maggiorenne per usare i vari servizi");
-        return false;
-    }
-    else return true;
-}
-function emailToRegister(){
-    if (document.getElementById("email").value==""){
-        alert("Inserire email");
+        invalidDate();
         return false;
     }
     else{
+        validDate();
+        return true;
+    }
+}
+function emailToRegister(){
+    if (document.getElementById("email").value==""){
+        invalidEmail();
+        return false;
+    }
+    else{
+        validEmail();
         if (document.getElementById("email").value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) return true;
         else{
-            alert("Email invalida");
+            invalidEmail();
             return false;
         }
     }
 }
 function validTelephone(){
     if (document.getElementById("telefono").value==""){
-        alert("Inserire numero di cellulare");
+        invalidTel();
         return false;
     }
-    console.log(document.getElementById("telefono").value);
+    validTel();
     if (document.getElementById("telefono").value.match(/^((\+|00)?39)?3\d{2}\d{6,7}$/)) return true;
     else {
-        alert("Inserire un numero di telefono valido");
+        invalidTel();
         return false;
     }
 }
 function comparePws(){
     var p1=document.getElementById("password1").value;
     var p2=document.getElementById("password2").value;
-    if (p1=="" || p2==""){
-        alert("Inserire password");
+    if (p1==""){
+        invalidP1();
+        return false;
+    }
+    if (p2==""){
+        invalidP2();
         return false;
     }
     if (p1!=p2){
-        alert("Le password inserite sono diverse");
+        invalidP1();
+        invalidP2();
         return false;
     }
+    validP1();
+    validP2();
     return true;
 }
 function check_lr(){
-    if (!validNameSurname() || !validBirthDate() || !emailToRegister() || !validTelephone() || !comparePws()) return false;
+    if (!nameIsValid() || !surnameIsValid() || !validBirthDate() || !emailToRegister() || !validTelephone() || !comparePws()) return false;
     return true;
 }
